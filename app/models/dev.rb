db826437871ec473a715849028453356b5ae7146
Jonathan Eatroff <jonathan.eatroff@gmail.com>
2023-01-17 09:57:37 -0500

    commit tree fea45f6688e828e931143226c7069ced09b9db5a

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,19 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        if (Freebie.find_by item_name: item_name) == nil
+            return false
+        else
+            return true
+        end
+    end
+
+    /doesn't work/
+    def give_away(dev, freebie)
+        if freebie.dev_id == self.id
+            freebie.dev_id = dev.id
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,17 @@
+Freebie.destroy_all
+Company.destroy_all
+Dev.destroy_all
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+dunder_mifflin = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+mr_meseeks = Dev.create(name: "Mr. Meseeks")
+gazorpazop = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +20,7 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+swag_bag = Freebie.create(item_name: "Swag Bag", value: 10, company_id: google.id, dev_id: gazorpazop.id)
+sweatshirt = Freebie.create(item_name: "Sweatshirt", value: 5, company_id: enron.id, dev_id: gazorpazop.id)
 
 puts "Seeding done!"
